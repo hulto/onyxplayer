761c2e4ce81cac4f3227ba13bb561534458d1638
dphan <ducphanduy.agentp@gmail.com>
2018-02-02 14:04:22 -0500

    Add things to the script

--- centos7/gcc.sh
+++ centos7/gcc.sh
@@ -15,4 +15,58 @@ changePasswords() {
     getActiveUsers | xargs -d'\n' -I {} sh -c "echo {}:$newp4ss | chpasswd"
 }
 
-changePasswords
+backupMemes() {
+    echo "backing up all the memes"
+    # Backup logs
+
+    # Backup configurations
+    # Backup binaries
+    # Backup critial files and command output
+}
+
+configMemes() {
+    echo "configuring all the memes"
+
+    # config SSH
+    configSSH
+}
+
+configSSH() {
+    echo "configuring SSH"
+}
+
+secureMemes() {
+    echo "securing all the memes"
+    secureLogs
+}
+
+secureLogs() {
+    echo "securing logs"
+    chattr +a -R /var/log/*
+}
+
+installMemes() {
+    if [ -n "$(command -v yum)" ]; then
+        # yum -y update
+        echo "yes"
+    elif [ -n "$(command -v apt-get)" ]; then
+        # apt-get -y update
+        echo "yes"
+    fi
+}
+
+firewallUp() {
+    echo "Now, bow to my firewall"
+    sh -c ./tables.sh
+}
+
+main() {
+    changePasswords
+    backupMemes
+    secureMemes
+    configMemes
+    installMemes
+    firewallUp
+}
+
+main "$@"
