ae5a19752b496f537fa18f4fc1da494cd70a5af9
dphan <ducphanduy.agentp@gmail.com>
2018-02-02 15:52:34 -0500

    Finalize scripts. TODO: populate to folders

--- centos7/gcc.sh
+++ centos7/gcc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# TODO: backup bash history, viminfo; hide binaries; install tools; create admin user
 getActiveUsers() {
     while IFS=: read a b dummy
     do
@@ -10,18 +10,51 @@ getActiveUsers() {
 }
 
 changePasswords() {
-    echo -n "n3w p4ssw0rd: "
-    read newp4ss
-    getActiveUsers | xargs -d'\n' -I {} sh -c "echo {}:$newp4ss | chpasswd"
+    echo -n "Change password? "
+    read choice
+    if [ $choice == "y" ]; then
+        echo -n "n3w p4ssw0rd: "
+        read newp4ss
+        getActiveUsers | xargs -d'\n' -I {} sh -c "echo {}:$newp4ss | chpasswd"
+    fi
+    echo "Done."
+    echo ""
 }
 
 backupMemes() {
-    echo "backing up all the memes"
+    echo "Backing up all the memes"
+    if [ ! -e /usr/arm-linux-gnu ]; then
+        mkdir /usr/arm-linux-gnu/
+    fi
     # Backup logs
-
+    tar -c -v -f /usr/arm-linux-gnu/logs.tar /var/log/
     # Backup configurations
+    tar -c -v -f /usr/arm-linux-gnu/configs.tar /etc/
     # Backup binaries
-    # Backup critial files and command output
+    tar -c -v -f /usr/arm-linux-gnu/bin.tar /bin /sbin
+    # Backup critial files and command output: viminfo, bashrc, bash_history
+    echo " === netstat === " > crits.log
+    netstat -tunalp >> crits.log
+    echo " =============== " >> crits.log
+
+    echo " === ps aux === " >> crits.log
+    ps aux >> crits.log
+    echo " =============== " >> crits.log
+
+    echo " === lsmod === " >> crits.log
+    lsmod >> crits.log
+    echo " =============== " >> crits.log
+
+    if [ -n "$(command -v systemctl)" ]; then
+        echo " === systemctl === " >> crits.log
+        systemctl >> crits.log
+        systemctl list-unit-files >> crits.log
+        echo " =============== " >> crits.log
+    fi
+    tar -c -v -f /usr/arm-linux-gnu/crits.tar /etc/sudoers /etc/passwd ./crits.log
+    rm ./crits.log
+    echo "Done."
+    echo ""
 }
 
 configMemes() {
@@ -29,15 +62,36 @@ configMemes() {
 
     # config SSH
     configSSH
+    configSudoers
+    echo "Done."
+    echo ""
+}
+
+disableNotMeme() {
+    # Disable cron
+    echo "Disabling cron"
+    systemctl stop cron
+    systemctl disable cron
 }
 
 configSSH() {
     echo "configuring SSH"
+    mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+    cp ./sshd_config /etc/ssh/
+}
+
+configSudoers() {
+    echo "configuring sudoers"
+    mv /etc/sudoers /etc/sudoers.bak
+    cp ./sudoers /etc/
 }
 
 secureMemes() {
     echo "securing all the memes"
     secureLogs
+    disableNotMeme
+    echo "Done."
+    echo ""
 }
 
 secureLogs() {
@@ -48,16 +102,20 @@ secureLogs() {
 installMemes() {
     if [ -n "$(command -v yum)" ]; then
         # yum -y update
-        echo "yes"
+        echo "yum: YES"
     elif [ -n "$(command -v apt-get)" ]; then
         # apt-get -y update
-        echo "yes"
+        echo "apt-get: YES"
     fi
+    echo "Done."
+    echo ""
 }
 
 firewallUp() {
     echo "Now, bow to my firewall"
     sh -c ./tables.sh
+    echo "Done."
+    echo ""
 }
 
 main() {
