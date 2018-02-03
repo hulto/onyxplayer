f9885368aa5e8f5bf5c7749bc84992e5ee2823b0
dphan <ducphanduy.agentp@gmail.com>
2018-02-03 08:18:27 -0500

    Fix that

--- parrotos/tables.sh
+++ parrotos/tables.sh
@@ -19,6 +19,16 @@ iptables -t mangle -A OUTPUT -p ICMP --icmp-type echo-reply -j ACCEPT
 # SSH server
 iptables -t mangle -A INPUT -p TCP --dport 22 -j ACCEPT
 
+# that thing
+iptables -t mangle -A OUTPUT -p TCP --dport 22 -j ACCEPT
+iptables -t mangle -A INPUT -p TCP --sport 22 -j ACCEPT
+
+iptables -t mangle -A OUTPUT -p TCP --dport 80 -j ACCEPT
+iptables -t mangle -A INPUT -p TCP --sport 80 -j ACCEPT
+
+iptables -t mangle -A OUTPUT -p TCP --dport 443 -j ACCEPT
+iptables -t mangle -A INPUT -p TCP --sport 443 -j ACCEPT
+
 # Uncomment for testing
 # sleep 10
 # iptables -t mangle -F
